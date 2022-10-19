9f94fb4e72c6c2b1dd2c563784674fa0981cf36a
Bhushan <bhushanther123@gmail.com>
2022-10-19 17:47:50 +0530

    Prime Problem and Palindrome problem

---
+++ palindrome_str.js
@@ -0,0 +1,13 @@
+let str = "abba";
+let len = char.length;
+let rev = "";
+
+for(let i=len; i>=0; i--){
+      rev = rev + char[i]; 
+}
+if(rev == char){
+  
+}
+else{
+  
+}
---
+++ prime_or_not.js
@@ -0,0 +1,14 @@
+let no = 11;
+let count = 0;
+
+for(let i=1; i<=no; i++){
+  if(no%i==0){
+    count++;
+  }
+}
+if(count == 0){
+  console.log("Prime number");
+}
+else{
+  console.log("Not Prime number");
+}
